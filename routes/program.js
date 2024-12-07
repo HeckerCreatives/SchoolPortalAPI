e7541c64b1942d1cce1af3810834ee660cc4ba70
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2024-12-07 09:28:20 +0800

    pooosh

--- routes/program.js
+++ routes/program.js
@@ -1,9 +1,12 @@
 const router = require("express").Router()
-const { getAllPrograms, CreateProgram } = require("../controllers/program");
+const { getAllPrograms, CreateProgram, getProgramEnrollmentSchedules } = require("../controllers/program");
 
 
 router
 .get("/getallprogram", getAllPrograms)
 .post("/createprogram", CreateProgram)
+
+//landing page
+.get("/getallprogramsenrollmentschedule", getProgramEnrollmentSchedules)
 
 module.exports = router;

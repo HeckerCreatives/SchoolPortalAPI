a21692cdc56fa5894abd1f95b38ca6d29eb27871
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-01-14 14:40:59 +0800

    poooosh

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const http = require("http");
 const cors = require("cors");
 require("dotenv").config();
 const { initialize } = require("./initialization/initialize");
+const sockethandler = require("./socket/socket")
 
 const app = express();
 
@@ -36,38 +37,7 @@ app.use(bodyParser.urlencoded({ limit: "50mb", extended: false, parameterLimit: 
 app.use(cookieParser());
 
 
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
-
-  // Handle joining a room
-  socket.on('Join_room', (conversationId) => {
-    if (!conversationId) {
-      return socket.emit('error', { message: 'Conversation ID is required to join a room.' });
-    }
-
-    socket.join(conversationId);
-    console.log(`User ${socket.id} joined room: ${conversationId}`);
-    socket.emit('room_joined', { roomId: conversationId });
-  });
-
-  socket.on('Send_message', ({ conversationId, message, sender }) => {
-    if (!conversationId || !message || !sender) {
-      return socket.emit('error', { message: 'Conversation ID, sender, and message are required.' });
-    }
-
-    console.log(`Message from ${sender} in room ${conversationId}: ${message}`);
-
-    socket.to(conversationId).emit('Receive_message', { sender, message });
-  });
-
-
-  socket.on('disconnect', () => {
-    console.log('User disconnected:', socket.id);
-  });
-});
-
-
-
+sockethandler(io)
 // Routes
 require("./routes")(app);
 

57fa1902d183251df79cc4725c43a6f38b41c1bc
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-01-18 16:45:09 +0800

    pooosh

--- routes/assignment.js
+++ routes/assignment.js
@@ -1,12 +1,23 @@
 const { createassignment, submitassignment, getassignments } = require("../controllers/assignment")
 const { protectteacheradviser, protectstudent } = require("../middleware/middleware")
+const upload = require("../middleware/upload")
+
+const uploadimg = upload.single("file")
+
 
 const router = require("express").Router()
 
 router
 .post("/createassignment", protectteacheradviser, createassignment)
 .get("/getassignmentsteacher", protectteacheradviser, getassignments)
-.post("/submitassignment", protectstudent, submitassignment)
+.post("/submitassignment", protectstudent, function (req, res, next){
+    uploadimg(req, res, function(err){
+        if(err){
+            return res.status(400).send({ message: "failed", data: err.message })
+        }
+        next()
+    })
+}, submitassignment)
 .get("/getassignmentsstudent", protectstudent, getassignments)
 
 

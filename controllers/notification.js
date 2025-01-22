f92eaf5e3d7f2a7ebe1168c25b45ec31a1c7429a
Joshupau <97903490+Joshupau@users.noreply.github.com>
2025-01-22 16:09:41 +0800

    pooosh

--- controllers/notification.js
+++ controllers/notification.js
@@ -48,3 +48,23 @@ exports.readnotification = async (req, res) => {
         return res.status(400).json({ message: "bad-request", data: "There's a problem with the server! Please contact support for more details."})
     })
 }
+
+exports.deletereceiverfromnotification = async (req, res) => {
+    const { id } = req.user
+
+    
+    await Notification.updateMany(
+        { "receiver.userId": new mongoose.Types.ObjectId(id) },
+        { $pull: { receiver: { userId: new mongoose.Types.ObjectId(id) } } } 
+    )
+    .then(data => {
+        if(!data){
+            return res.status(400).json({ message: "failed", data: "No notifications found."})
+        }
+        return res.status(200).json({ message: "success" })
+    })
+    .catch(err => {
+        console.log(`There's a problem encountered while updating notifications. Error: ${err}`)
+        return res.status(400).json({ message: "bad-request", data: "There's a problem with the server! Please contact support for more details."})
+    })
+}
--- routes/notification.js
+++ routes/notification.js
@@ -1,4 +1,4 @@
-const { getnotifications, readnotification } = require("../controllers/notification")
+const { getnotifications, readnotification, deletereceiverfromnotification } = require("../controllers/notification")
 const { protectstudent, protectstaffusers } = require("../middleware/middleware")
 
 const router = require("express").Router()
@@ -8,5 +8,6 @@ router
 .get("/getstudentnotification", protectstudent, getnotifications)
 .get("/getstaffnotification", protectstaffusers, getnotifications)
 .get("/readnotification", protectstudent, readnotification)
+.get("/deletereceiverfromnotification", protectstudent, deletereceiverfromnotification)
 
 module.exports = router
